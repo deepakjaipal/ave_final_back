8dd1cc6e163472b8df7a939dd57593b8773fff5e
deepakjaipal <108461944+deepakjaipal@users.noreply.github.com>
2025-12-20 20:46:11 +0530

    Add getUserById route to userRoutes

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile, getUsers, updateUser, deleteUser, getUserStats } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile, getUsers, updateUser, deleteUser, getUserStats,  getUserById } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.route('/stats').get(protect, admin, getUserStats);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
-router.route('/:id').put(protect, admin, updateUser).delete(protect, admin, deleteUser);
+router.route('/:id').get(protect, admin, getUserById).put(protect, admin, updateUser).delete(protect, admin, deleteUser);
 
 export default router;
